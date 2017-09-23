50f789d4176fb8241cfd5409d6e04b0222443755
dr0l3 <runedrolepedersen@gmail.com>
2017-09-23 19:49:06 +0200

    Add idrs and atom

---
+++ atom/install_atom.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update
+sudo apt install atom
---
+++ idris/install_idris.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo apt-get install cabal-install
+cabal update
+cabal install idris
