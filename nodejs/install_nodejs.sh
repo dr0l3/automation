22bab4ced8f340c5d107c069b4f33e8acf4cb5c6
Rune Pedersen <rdp@dixa.com>
2017-12-04 15:35:48 +0100

    corrects npm install change of path

--- nodejs/install_nodejs.sh
+++ nodejs/install_nodejs.sh
@@ -2,7 +2,7 @@
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 mkdir "${HOME}/.npm-packages"
-echo "prefix=${HOME}/.npm.packages" >> ~/.npmrc
+echo "prefix=${HOME}/.npm-packages" >> ~/.npmrc
 
 echo "NPM_PACKAGES=\"${HOME}/.npm-packages\"" >> ~/.profile
 echo "PATH=\"$NPM_PACKAGES/bin:$PATH\"" >> ~/.profile
